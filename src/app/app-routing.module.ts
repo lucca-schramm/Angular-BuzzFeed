c4ee726c294287320bc0904965541c31538670c3
Lucca Schramm <luccascho@gmail.com>
2023-05-26 14:01:53 -0300

    corrigindo roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent}
 ];
 
 @NgModule({
